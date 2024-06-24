a7b04832c846d828fde2408212786ef20814438a
Venkat-Sumanth <sumanthbethanapalli12@gmail.com>
2024-06-24 14:02:44 +0530

    API

---
+++ Python Practise/demo.py
@@ -0,0 +1,29 @@
+k=77
+b=88
+print('python programming language is th best')
+print(a+b)
+def subscribe(a):
+    return 'like a video'
+v=subscribe(1)
+print(v)
+
+print('share the video')
+print(2+3)
+
+print('Python programming language')
+def subscription():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return 'thank you'
+
+if True:
+    print('This is book')
+
+print('please subscribe')
+k=66
+b=77
+print(k+b)
+
